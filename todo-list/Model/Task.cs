0d1fb86ae2080f0a0d5abbe0510071f3fe4d1dad
Szymon Grzeszczuk <szymon3520@gmail.com>
2025-02-02 20:10:21 +0100

    Added MVVM structure and displaying, adding and deleting tasks.

---
+++ todo-list/Model/Task.cs
@@ -0,0 +1,25 @@
+﻿namespace todo_list.Model
+{
+    public class Task
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Created { get; set; }
+
+        public Task(string title, string description, string id = null)
+        {
+            Title = title;
+            Description = description;
+
+            Created = DateTime.Now;
+
+            if (id != null)
+                Id = id;
+            else
+                Id = Guid.NewGuid().ToString("D");
+        }
+
+
+    }
+}
